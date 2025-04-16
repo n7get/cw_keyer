c7f3df312f64fe11312990eef77b80dd12e88a8b
Robert Ambrose <rna@muttsoft.com>
2025-04-16 12:22:23 -0700

    Changed to move to pins on a ESC32c3.

--- main/pins.h
+++ main/pins.h
@@ -3,8 +3,8 @@
 // - GPIO7 (MISO)
 // - GPIO8 (MOSI)
 // - GPIO11 (CS)
-#define BUTTON_GPIO_PIN 11
-#define KEY_GPIO_PIN 2
-#define LED_GPIO_PIN 3
-#define RXD_PIN 4
-#define TXD_PIN 5
+#define BUTTON_GPIO_PIN 2
+#define KEY_GPIO_PIN 3
+#define LED_GPIO_PIN 4
+#define RXD_PIN 10
+#define TXD_PIN 9
